4805bda0f80abe8f53a6db61c6fcdf30c5de882e
Chaosgh <149420543+Chaosgh@users.noreply.github.com>
2025-11-19 15:46:10 +0000

    Deploying to gh-pages from @ TransientCodes/TransientWiki@0e329d4814b2cb030ad3ce229dac8aca90a6bfc4 🚀

---
+++ assets/home-gV0FlA66.js
@@ -0,0 +1,42 @@
+const e=`# **TransientRealm – Offizielles Wiki**
+
+Willkommen im offiziellen TransientRealm-Wiki.
+Hier findest du alle Informationen über die Inhalte, Systeme und Features unseres Servers.
+
+---
+
+## **Was ist TransientRealm?**
+
+TransientRealm ist ein deutscher Minecraft-Server mit Schwerpunkt auf **Citybuild**, ergänzt durch **eigene Systeme**, **magische Berufe**, **Dungeons**, **Spezial-Crafting** und einer stetig wachsenden, atmosphärischen Welt.
+Der Server kombiniert klassisches Bauen mit ersten RPG-Elementen und individuellen Gameplay-Mechaniken.
+
+Unser Ziel ist eine einzigartige Welt, die kreatives Bauen, Fortschritt, Erkundung und Handel miteinander verbindet.
+
+---
+
+## **Welche Inhalte bietet TransientRealm?**
+
+Dieses Wiki beschreibt alle aktuellen Spielsysteme, darunter:
+
+* Citybuild-System
+* Jobs-System
+* Siegelmagier-System & Siegelaltar
+* Dungeon- & Maze-System
+* Clan-System
+* Fortschritts- und Belohnungssystem
+
+Ebenso findest du Informationen zu:
+
+* Währungen
+* Items und Materialien
+* Wichtigen Befehlen
+* Zukünftigen Erweiterungen
+
+---
+
+## **Zweck dieses Wikis**
+
+Dieses Wiki dient als zentraler Einstiegspunkt in die Welt von TransientRealm.
+Von hier aus gelangst du zu allen Unterseiten, die die Systeme und Mechaniken im Detail erklären.
+
+`;export{e as default};
