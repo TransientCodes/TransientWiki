17962cb142d81ac32bb07458c4e8c9305b56d0ae
Chaosgh <149420543+Chaosgh@users.noreply.github.com>
2026-04-25 22:20:32 +0000

    Deploying to gh-pages from @ TransientCodes/TransientWiki@c16b17cef70dfdf8241f105178a9de815db276e3 🚀

--- assets/befehle-BI1zojev.js
+++
@@ -1,9 +0,0 @@
-const e=`<div class="md-banner" style="border-left: 4px solid #aa55ff;">
-    <h1 style="margin: 0;">Kult-Befehle</h1>
-</div>
-
-| Befehl | Beschreibung |
-|---|---|
-| \`/kult\` | Kult-Menü öffnen |
-| \`/kult donate <Betrag>\` | Pfund in die Kult-Bank einzahlen (wird zu Rubinen) |
-`;export{e as default};
